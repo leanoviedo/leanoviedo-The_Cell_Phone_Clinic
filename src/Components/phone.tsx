26ed2dc93d035ab7a893a9f515ccc24433751a76
leandro oviedo <leanoviedo@gmail.com>
2024-06-01 02:28:54 -0300

    create-component-phone

---
+++ src/Components/phone.tsx
@@ -0,0 +1,13 @@
+import { Grid } from "@mui/material";
+import  { Component } from "react";
+
+const phone = () => {
+  return <>
+  <Grid component={Component}>
+
+  </Grid>
+
+  </>;
+};
+
+export default phone;
